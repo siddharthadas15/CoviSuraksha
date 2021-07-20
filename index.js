2214616c0e805d087b08ec50e048d7365cc17eec
siddharthadas15 <sd78268@gmail.com>
2021-07-21 04:37:50 +0530

    common security issues fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
 
-
 const express=require('express');
 const app=express();
 const path=require('path');
@@ -10,8 +9,10 @@ const mongoose=require('mongoose');
 const flash=require('connect-flash');
 const session=require('express-session');
 const methodOverride=require('method-override');
+const helmet=require('helmet');
 const ejsMate=require('ejs-mate');
 const passport=require('passport');
+const mongoSanitize = require('express-mongo-sanitize');
 const LocalStrategy=require('passport-local');
 const Post=require('./models/post');
 const Review=require('./models/review');
@@ -23,7 +24,10 @@ const reviewRoutes=require('./routes/reviews');
 const userRoutes=require('./routes/user');
 const User=require('./models/user');
 
-mongoose.connect('mongodb://localhost:27017/CoviSuraksha', { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true })
+const MongoDBStore = require("connect-mongo")(session);
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/CoviSuraksha';
+//'mongodb://localhost:27017/CoviSuraksha'
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true })
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -39,9 +43,75 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(mongoSanitize());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/sd15/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+              "https://images.unsplash.com/",
+              "https://images.pexels.com/photos/3786124/pexels-photo-3786124.jpeg",
+              "https://images.pexels.com/photos/3786120/pexels-photo-3786120.jpeg",
+              "https://www.hnl.physio/wp-content/uploads/2020/04/rm208batch7-adj-11.png"
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
+
+const secret=process.env.SECRET||'#2467#9090*/324##configsessionuid15';
+
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60
+});
+
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig={
-  secret:'#2467#9090*/324##configsessionuid15',
+  store,
+  name:'azy1HlAkX367nKq',
+  secret,
+  // secure:true,
   resave:false,
   saveUninitialized:true,
   cookie:{
@@ -58,7 +128,9 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
+// console.log(req.query);
 res.locals.currentUser=req.user;
 res.locals.success=req.flash('success');
 res.locals.error=req.flash('error');
@@ -113,8 +185,8 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error', { err })
 });
 
+const PORT=process.env.PORT||3000;
 
-
-app.listen('3000',()=>{
+app.listen(PORT,()=>{
 console.log('Server Started!!!');
 });
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -7,18 +7,13 @@ c='mapbox://styles/mapbox/light-v10';
 var map = new mapboxgl.Map({
 container: 'cluster-map',
 style:c,
-center: [-103.59179687498357, 40.66995747013945],
+center: [77.2090212,28.6139391],
 zoom: 3
 });
 map.addControl(new mapboxgl.NavigationControl());
 map.on('load', function () {
-// Add a new source from our GeoJSON data and
-// set the 'cluster' option to true. GL-JS will
-// add the point_count property to your source data.
 map.addSource('posts', {
 type: 'geojson',
-// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
 data: posts,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
@@ -31,11 +26,6 @@ type: 'circle',
 source: 'posts',
 filter: ['has', 'point_count'],
 paint: {
-// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-// with three steps to implement three types of circles:
-//   * Blue, 20px circles when point count is less than 100
-//   * Yellow, 30px circles when point count is between 100 and 750
-//   * Pink, 40px circles when point count is greater than or equal to 750
 'circle-color': [
 'step',
 ['get', 'point_count'],
@@ -101,18 +91,11 @@ zoom: zoom
 );
 });
  
-// When a click event occurs on a feature in
-// the unclustered-point layer, open a popup at
-// the location of the feature, with
-// description HTML from its properties.
 map.on('click', 'unclustered-point', function (e) {
     const { popUpMarkup } = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
 
  
-// Ensure that if the map is zoomed out such that
-// multiple copies of the feature are visible, the
-// popup appears over the copy being pointed to.
 while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 }
